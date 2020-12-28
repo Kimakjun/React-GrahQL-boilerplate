c7ae9a37656fb9b774d27486e3a67f6f744577b2
dkd7142@naver,com <dkd7142@naver.com>
2020-12-28 23:36:50 +0900

    Chore #noissue : cluster mode 로 server 실행.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,26 @@
+/* eslint-disable no-plusplus */
+import cluster from 'cluster';
+import os from 'os';
 import db from '@model/.';
 import app from './app';
 
+const cpuNums = os.cpus().length;
+
 db();
 
-app.server.listen(8000, () => console.log('server started at PORT:8000'));
+if (cluster.isMaster) {
+  console.log('Master is running!');
+
+  for (let i = 0; i < cpuNums; i++) {
+    console.log('cluster fork!');
+    cluster.fork();
+  }
+
+  cluster.on('exit', function (worker, code, signal) {
+    if (code === 200) {
+      cluster.fork();
+    }
+  });
+} else {
+  app.server.listen(8000, () => console.log('server started at PORT:8000'));
+}
